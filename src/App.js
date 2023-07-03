147af48736d1e31ec6d46b0bc3bb0f30c3e38cf8
midia-hamdi <midiahamdi367@gmail.com>
2023-07-03 18:43:17 +0330

    add components

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import CleanChat from "./components/CleanChat";
+import "./Style.css"
+
+function App (){
+    return <CleanChat />
+}
+
+export default App;
---
+++ src/components/CleanChat.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Heading from "./Heading";
+import Body from "./Body";
+import Footer from "./Footer";
+
+export default class CleanChat extends Component {
+  render() {
+    return (
+        <div className="container bootstrap snippets">
+            <div className="col-md-7 col-xs-12 col-md-offset-2">
+            <div className="panel" id="chat">
+
+
+
+            </div>
+            </div>
+        </div>
+    )
+  }
+}
